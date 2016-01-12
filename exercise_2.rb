520f20264239ba56cc06452c32e1eb6c1b707353
naemcivic <naemcivic@gmail.com>
2016-01-12 14:08:53 -0500

    compelete second excersie

---
+++ exercise_2.rb
@@ -0,0 +1,10 @@
+#the way to tip above average/good tip
+puts "This is the calculation (55 * 0.2)for a great tip based on a $55 meal"
+#using interpolation to print integer and strings together
+puts "$#{1.to_s}"
+#another way to acomplish the same result as on line 4
+puts "$"+1.to_s
+#using string interpolation to complete an arithmatic operation
+puts "#{(45628 * 7839)}"
+#The boolean value of this comparison is "true"
+(10 < 20 && 30 < 20) || !(10 == 11)
