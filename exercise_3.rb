7163cd7312ddcfe9bd02121017d45b709830fb2c
naemcivic <naemcivic@gmail.com>
2016-01-12 14:48:09 -0500

    compeleted exercise 3

--- exercise_3.rb
+++ exercise_3.rb
@@ -1,2 +1,6 @@
 #use puts command to output string to screen
 puts "What is your name?"
+#create variable 'username' and assigning it to get user input
+username = gets.chomp
+#puts a welcome message to the user on team Gort
+puts "hi Gort member #{username}"
