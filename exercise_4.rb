ebe61518e247b3398a4ad2957ae3cace70c2639e
naemcivic <naemcivic@gmail.com>
2016-01-12 15:05:58 -0500

    completed exercise 4 with 2 different approach! thats the beauty of ruby, different ways to solve the same problem

--- exercise_4.rb
+++ exercise_4.rb
@@ -14,3 +14,19 @@ while x <= 100
 #increments the variable by 1 on each turn
   x += 1
 end
+
+
+#here is another way of using each iteration to accomplish the same thing
+one2hundred = 1..100
+
+one2hundred.each do |numbers|
+  if numbers % 15 == 0
+    puts "Bitmaker Labs"
+  elsif numbers % 3 == 0
+    puts "Bitmaker"
+  elsif numbers % 5 == 0
+    puts "Labs"
+  else
+    puts numbers
+  end
+end
